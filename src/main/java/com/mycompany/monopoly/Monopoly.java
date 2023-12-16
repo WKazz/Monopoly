a693e4646c33788b5ebf3cdc60e6a4228324a39f
Wkazz <nalexesp@gmail.com>
2023-12-16 20:00:39 +0100

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/monopoly/Monopoly.java
@@ -0,0 +1,12 @@
+package com.mycompany.monopoly;
+
+
+import ui.VentanaMenu;
+public class Monopoly {
+
+    public static void main(String[] args) {
+        
+        VentanaMenu ventana1 = new VentanaMenu();
+        ventana1.setVisible(true);
+    }
+}
